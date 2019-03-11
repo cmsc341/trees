cf97e7bb272d0b023c33ad75db68a597d1de42f1
Ben <doc.ben.johnson@gmail.com>
2019-03-11 14:16:44 -0400

    driver to save time on review day

--- bst/driver.cpp
+++ bst/driver.cpp
@@ -2,6 +2,18 @@
 // Created by Ben Johnson on 2019-03-11.
 //
 
+#include <cstdlib>
+#include "BST.h"
+
 int main() {
     // we write this first because we're awesome
+    BST tree;
+    for (int i = 0; i < 100; ++i) {
+        tree.insert(i);
+    }
+
+    BST randomTree;
+    for (int j = 0; j < 100; ++j) {
+        tree.insert(rand() % 1000);
+    }
 }
